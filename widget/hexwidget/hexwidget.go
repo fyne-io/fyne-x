df17679c1ae6d6f2f3ad755f0ab930a11340f923
Charles Daniels <charles@cdaniels.net>
2021-02-28 00:39:03 -0500

    address Andy's feedback for hexwidget

--- widget/hexwidget/hexwidget.go
+++ widget/hexwidget/hexwidget.go
@@ -96,6 +96,7 @@ func (h *HexWidget) getSegmentColor(segno int) color.RGBA {
 
 // CreateRenderer implements fyne.Widget
 func (h *HexWidget) CreateRenderer() fyne.WidgetRenderer {
+
 	seg0 := canvas.NewLine(hexOffColor)
 	seg1 := canvas.NewLine(hexOffColor)
 	seg2 := canvas.NewLine(hexOffColor)
@@ -114,7 +115,6 @@ func (h *HexWidget) CreateRenderer() fyne.WidgetRenderer {
 
 	pos := image.Pt(0, 0)
 
-	// pt0Center := image.Pt(pos.X+int(hexWidth/2.0+hexOffset), pos.Y+int((hexHeight-2.0*hexSegmentVLength)/2))
 	pt0Center := image.Pt(pos.X+int(hexWidth/2.0+hexOffset), pos.Y)
 	pt05 := image.Pt(int(float32(pt0Center.X)-(hexSegmentHLength/2)), pt0Center.Y)
 	pt01 := image.Pt(int(float32(pt0Center.X)+(hexSegmentHLength/2)), pt0Center.Y)
@@ -127,25 +127,12 @@ func (h *HexWidget) CreateRenderer() fyne.WidgetRenderer {
 	pt34 := image.Pt(int(float32(pt3Center.X)-(hexSegmentHLength/2)), pt3Center.Y)
 	pt32 := image.Pt(int(float32(pt3Center.X)+(hexSegmentHLength/2)), pt3Center.Y)
 
-	// segment 0
 	setLineEndpoints(seg0, pt05, pt01)
-
-	// segment 1
 	setLineEndpoints(seg1, pt01, pt61)
-
-	// segment 2
 	setLineEndpoints(seg2, pt61, pt32)
-
-	// segment 3
 	setLineEndpoints(seg3, pt32, pt34)
-
-	// segment 4
 	setLineEndpoints(seg4, pt34, pt65)
-
-	// segment 5
 	setLineEndpoints(seg5, pt65, pt05)
-
-	// segment 6
 	setLineEndpoints(seg6, pt65, pt61)
 
 	return &hexRenderer{
@@ -165,9 +152,9 @@ func NewHexWidget() *HexWidget {
 	return h
 }
 
-// UpdateSegments changes the state of the segments and causes the widget to
+// updateSegments changes the state of the segments and causes the widget to
 // refresh so the changes are visible to the user.
-func (h *HexWidget) UpdateSegments(segments uint8) {
+func (h *HexWidget) updateSegments(segments uint8) {
 	h.segments = segments
 	h.Refresh()
 }
@@ -179,36 +166,36 @@ func (h *HexWidget) Set(val uint) {
 	val = val % 16
 
 	if val == 0 {
-		h.UpdateSegments(1 << 6)
+		h.updateSegments(1 << 6)
 	} else if val == 1 {
-		h.UpdateSegments((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 6) | (1 << 7))
+		h.updateSegments((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 6) | (1 << 7))
 	} else if val == 2 {
-		h.UpdateSegments((1 << 2) | (1 << 5))
+		h.updateSegments((1 << 2) | (1 << 5))
 	} else if val == 3 {
-		h.UpdateSegments((1 << 4) | (1 << 5))
+		h.updateSegments((1 << 4) | (1 << 5))
 	} else if val == 4 {
-		h.UpdateSegments((1 << 0) | (1 << 3) | (1 << 4))
+		h.updateSegments((1 << 0) | (1 << 3) | (1 << 4))
 	} else if val == 5 {
-		h.UpdateSegments((1 << 1) | (1 << 4))
+		h.updateSegments((1 << 1) | (1 << 4))
 	} else if val == 6 {
-		h.UpdateSegments((1 << 1))
+		h.updateSegments((1 << 1))
 	} else if val == 7 {
-		h.UpdateSegments((1 << 3) | (1 << 4) | (1 << 5) | (1 << 6))
+		h.updateSegments((1 << 3) | (1 << 4) | (1 << 5) | (1 << 6))
 	} else if val == 8 {
-		h.UpdateSegments(0)
+		h.updateSegments(0)
 	} else if val == 9 {
-		h.UpdateSegments((1 << 3) | (1 << 4))
+		h.updateSegments((1 << 3) | (1 << 4))
 	} else if val == 10 {
-		h.UpdateSegments((1 << 3))
+		h.updateSegments((1 << 3))
 	} else if val == 11 {
-		h.UpdateSegments((1 << 0) | (1 << 1))
+		h.updateSegments((1 << 0) | (1 << 1))
 	} else if val == 12 {
-		h.UpdateSegments((1 << 0) | (1 << 1) | (1 << 2) | (1 << 5))
+		h.updateSegments((1 << 0) | (1 << 1) | (1 << 2) | (1 << 5))
 	} else if val == 13 {
-		h.UpdateSegments((1 << 0) | (1 << 5))
+		h.updateSegments((1 << 0) | (1 << 5))
 	} else if val == 14 {
-		h.UpdateSegments((1 << 1) | (1 << 2))
+		h.updateSegments((1 << 1) | (1 << 2))
 	} else if val == 15 {
-		h.UpdateSegments((1 << 1) | (1 << 2) | (1 << 3))
+		h.updateSegments((1 << 1) | (1 << 2) | (1 << 3))
 	}
 }
