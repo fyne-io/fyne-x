246633efe487fa1a607357c1ad3384901dbf0ae7
Jacalz <jacob.alzen@gmail.com>
2021-04-26 18:02:59 +0200

    Add a password validator

---
+++ data/validation/password.go
@@ -0,0 +1,18 @@
+// Package validation provides validation for data inside widgets
+package validation // import "fyne.io/x/fyne/data/validation"
+
+import (
+	"fyne.io/fyne/v2"
+	gpv "github.com/wagslane/go-password-validator"
+)
+
+// NewPassword returns a new validator for validating passwords.
+// Validate returns nil if the password entropy is greater than or equal
+// to the minimum entropy. If not, an error is returned that explains
+// how the password can be strengthened. Advice on entropy value:
+// https://github.com/wagslane/go-password-validator/tree/main#what-entropy-value-should-i-use
+func NewPassword(minEntropy float64) fyne.StringValidator {
+	return func(text string) error {
+		return gpv.Validate(text, minEntropy)
+	}
+}
