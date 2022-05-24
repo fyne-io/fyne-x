d3c2689b214fb28f859b1a585bd34267bd2eef7c
Patrice Ferlet <metal3d@gmail.com>
2022-05-24 06:20:56 +0200

    Add a demo for desktop integration

---
+++ cmd/desktop_demo/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	xtheme "fyne.io/x/fyne/theme"
+	"fyne.io/x/fyne/theme/desktop"
+)
+
+func main() {
+	app := app.New()
+	app.Settings().SetTheme(xtheme.FromDesktopEnvironment())
+	win := app.NewWindow("Desktop integration demo")
+	win.Resize(fyne.NewSize(550, 390))
+	win.CenterOnScreen()
+
+	entry := widget.NewEntry()
+	entry.SetPlaceHolder("Type something here")
+	win.SetContent(container.NewBorder(
+		nil,
+		container.NewHBox(
+			widget.NewButtonWithIcon("Home icon button", theme.HomeIcon(), nil),
+			widget.NewButtonWithIcon("Info icon button", theme.InfoIcon(), nil),
+			widget.NewButtonWithIcon("Example file dialog", theme.FolderIcon(), func() {
+				dialog.ShowFileSave(func(fyne.URIWriteCloser, error) {}, win)
+			}),
+		),
+		nil,
+		nil,
+		container.NewVBox(
+			createExplanationLabel(app),
+			entry,
+		),
+	))
+
+	win.ShowAndRun()
+}
+
+func createExplanationLabel(app fyne.App) fyne.CanvasObject {
+
+	var current string
+
+	switch app.Settings().Theme().(type) {
+	case *desktop.GnomeTheme:
+		current = "Gnome / GTK"
+	case *desktop.KDETheme:
+		current = "KDE / Plasma"
+	default:
+		current = "This window manager is not supported for now"
+	}
+
+	text := "Current theme: " + current + "\n"
+	text += `
+
+This window should be styled to look like a desktop application. It works with GTK/Gnome based desktops and KDE/Plasma at this time
+For the others desktops, the application will look like a normal window with default theme.
+
+You may try to change icon theme or GTK/KDE theme in your desktop settings, as font, font scaling...
+
+Note that you need to have fontforge package to make Fyne able to convert non-ttf fonts to ttf.
+`
+	label := widget.NewLabel(text)
+	label.Wrapping = fyne.TextWrapWord
+	return label
+}
