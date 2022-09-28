a3744b376a14dd9aea033de0a67808819102d22f
Svart Adam Solander <adam.solander@gmail.com>
2022-09-28 11:33:16 +0200

    "NewDisableableBinding now returns an interface."

--- data/binding/disableableBinding.go
+++ data/binding/disableableBinding.go
@@ -7,7 +7,15 @@ import (
 	"fyne.io/fyne/v2/data/binding"
 )
 
-type disableableBinding struct {
+// DisableableBinding is a `Bool` binding with accept Disableable widgets that it will control the Disabled status of.
+type DisableableBinding interface {
+	binding.Bool
+
+	AddWidgets(...fyne.Disableable)
+	SetInverted(bool)
+}
+
+type boundDisableable struct {
 	binding.Bool
 
 	inverted bool
@@ -16,8 +24,8 @@ type disableableBinding struct {
 
 // NewDisableableBinding returns a `Bool` binding which accepts Disableable widgets.
 // When the Bool changes, the widgets Enable or Disable method will be executed.
-func NewDisableableBinding(widgets ...fyne.Disableable) *disableableBinding {
-	newBinding := &disableableBinding{
+func NewDisableableBinding(widgets ...fyne.Disableable) DisableableBinding {
+	newBinding := &boundDisableable{
 		Bool:    binding.NewBool(),
 		widgets: widgets,
 	}
@@ -30,19 +38,19 @@ func NewDisableableBinding(widgets ...fyne.Disableable) *disableableBinding {
 
 // Adding widgets to the binding.
 // This will update the Disable status of the widgets immediately.
-func (b *disableableBinding) AddWidgets(widgets ...fyne.Disableable) {
+func (b *boundDisableable) AddWidgets(widgets ...fyne.Disableable) {
 	b.widgets = append(b.widgets, widgets...)
 	b.update()
 }
 
 // SetInverted will switch the behavior of when the widgets will be Enabled or Disabled.
 // This will update the Disable status of the widgets immediately.
-func (b *disableableBinding) SetInverted(inverted bool) {
+func (b *boundDisableable) SetInverted(inverted bool) {
 	b.inverted = inverted
 	b.update()
 }
 
-func (b *disableableBinding) update() {
+func (b *boundDisableable) update() {
 	val, err := b.Get()
 	if err != nil {
 		log.Println(err)
