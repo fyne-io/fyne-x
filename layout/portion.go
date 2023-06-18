ac0ac93525ce125fbaef2211e9ac4a0e64f37165
Jacalz <Jacalz@users.noreply.github.com>
2023-06-18 13:44:01 +0200

    layout: Calculate portion MinSize from largest MinSize divided by portion

--- layout/portion.go
+++ layout/portion.go
@@ -37,18 +37,30 @@ func (p *HPortion) MinSize(objects []fyne.CanvasObject) fyne.Size {
 		return fyne.NewSize(0, 0)
 	}
 
-	size := fyne.Size{}
-	for _, item := range objects {
-		min := item.MinSize()
-		size.Width += min.Width
-		size.Height = fyne.Max(size.Height, min.Height)
+	if len(objects) == 0 {
+		return fyne.NewSize(0, 0)
 	}
 
-	return size
+	maxMinWidth := float32(0)
+	maxIndex := -1
+	height := float32(0)
+
+	for i := 0; i < len(objects); i++ {
+		min := objects[i].MinSize()
+		height = fyne.Max(height, min.Height)
+
+		if min.Width > maxMinWidth {
+			maxMinWidth = min.Width
+			maxIndex = i
+		}
+	}
+
+	return fyne.NewSize(maxMinWidth/p.Portions[maxIndex], height)
 }
 
 // NewHPortion creates a layout that partitions objects verticaly taking up
 // as large of a portion of the space as defined by the given slice.
+// The portions should be between 0 and 1 but not equal to.
 // The length of the Portions slice needs to be equal to the amount of objects.
 func NewHPortion(Portions []float32) *HPortion {
 	return &HPortion{Portions: Portions}
@@ -84,18 +96,30 @@ func (p *VPortion) MinSize(objects []fyne.CanvasObject) fyne.Size {
 		return fyne.NewSize(0, 0)
 	}
 
-	size := fyne.Size{}
-	for _, item := range objects {
-		min := item.MinSize()
-		size.Width = fyne.Max(size.Width, min.Width)
-		size.Height += min.Height
+	if len(objects) == 0 {
+		return fyne.NewSize(0, 0)
 	}
 
-	return size
+	maxMinHeight := float32(0)
+	maxIndex := -1
+	width := float32(0)
+
+	for i := 0; i < len(objects); i++ {
+		min := objects[i].MinSize()
+		width = fyne.Max(width, min.Width)
+
+		if min.Height > maxMinHeight {
+			maxMinHeight = min.Height
+			maxIndex = i
+		}
+	}
+
+	return fyne.NewSize(width, maxMinHeight/p.Portions[maxIndex])
 }
 
 // NewVPortion creates a layout that partitions objects verticaly taking up
 // as large of a portion of the space as defined by the given slice.
+// The portions should be between 0 and 1 but not equal to.
 // The length of the Portions slice needs to be equal to the amount of objects.
 func NewVPortion(portion []float32) *VPortion {
 	return &VPortion{Portions: portion}
