c739ddd59ceaa5ce4b487b7ed0afce7bd6968b96
Patrice Ferlet <metal3d@gmail.com>
2023-10-31 21:35:33 +0100

    Add doc

--- container/responsive_ratios.go
+++ container/responsive_ratios.go
@@ -3,23 +3,25 @@ package container
 // Ratio is a helper type to define a fraction of a container. It's a float32 alias.
 type Ratio = float32
 
-// Ful l is the full size of the container.
+// Some helpers to define common ratios.
+
+// FullRatio is the full size of the container.
 func FullRatio() Ratio { return 1.0 }
 
-// Half is the half size of the container.
+// HalfRatio is the half size of the container.
 func HalfRatio() Ratio { return 0.5 }
 
-// OneThird is the one third size of the container.
+// OneThirdRatio is the one third size of the container.
 func OneThirdRatio() Ratio { return 1.0 / 3.0 }
 
-// TwoThird is the two thirds size of the container.
+// TwoThirdRatio is the two thirds size of the container.
 func TwoThirdRatio() Ratio { return OneThirdRatio() * 2.0 }
 
-// OneQuarter is the one quarter size of the container.
+// OneQuarterRatio is the one quarter size of the container.
 func OneQuarterRatio() Ratio { return 0.25 }
 
-// ThreeQuarter is the three quarters size of the container.
+// ThreeQuarterRatio is the three quarters size of the container.
 func ThreeQuarterRatio() Ratio { return 0.75 }
 
-// OneFifth is the one fifth size of the container.
+// OneFifthRatio is the one fifth size of the container.
 func OneFifthRatio() Ratio { return 0.2 }
