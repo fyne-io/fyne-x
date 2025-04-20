6dfd13fe5f47aa776c1a63735813e6e2ac18cac7
Jim Orcheson <jimorcheson@gmail.com>
2025-04-20 11:31:25 -0400

    Add a demo program

---
+++ cmd/numerical_entry_demo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	xwidget "fyne.io/x/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("NumericalEntry Demo")
+
+	nE := xwidget.NewNumericalEntry()
+	nE.AllowFloat = true
+	nE.AllowNegative = true
+	w.Clipboard().SetContent("92.f65")
+
+	sep := widget.NewSeparator()
+	setB := widget.NewButton("Set Entry to -2.35,4", func() {
+		nE.SetText("-2.35,4")
+	})
+	appB := widget.NewButton("Append -3,6.5 to Entry", func() {
+		nE.Append("-3,6.5")
+	})
+	pasteL := widget.NewLabel("Clipboard contains \"92.f65\".\n" +
+		"Paste this into the Entry widget at\ndifferent locations to see the effect.")
+
+	vc := container.NewVBox(nE, sep, setB, appB, pasteL)
+	w.SetContent(vc)
+	w.ShowAndRun()
+}
